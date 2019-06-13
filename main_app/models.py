719cc061006ae3fd89f43b80247a1daf28618adb
mykindofscum <alvinkchan99@gmail.com>
2019-06-12 22:28:30 -0700

    Add add toy forms/functions/admin

--- main_app/models.py
+++ main_app/models.py
@@ -26,7 +26,7 @@ class Turtle(models.Model):
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
-    # toys = models.ManyToManyField(Toy)
+    toys = models.ManyToManyField(Toy)
 
 
     def __str__(self):
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Turtle, Photo, Toy
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 from .forms import FeedingForm
 import uuid
 import boto3
@@ -32,9 +33,10 @@ def turtles_index(request):
 
 def turtles_detail(request, turtle_id):
     turtle = Turtle.objects.get(id=turtle_id)
+    toys_turtle_doesnt_have = Toy.objects.exclude(id__in = turtle.toys.all().values_list('id'))
     feeding_form = FeedingForm()
     return render(request, 'turtles/detail.html', {
-         'turtle': turtle, 'feeding_form': feeding_form 
+         'turtle': turtle, 'toys': toys_turtle_doesnt_have, 'feeding_form': feeding_form 
     })
 
 def add_feeding(request, turtle_id):
@@ -43,6 +45,14 @@ def add_feeding(request, turtle_id):
         new_feeding = form.save(commit=False)
         new_feeding.turtle_id = turtle_id
         new_feeding.save()
+    return redirect('detail', turtle_id=turtle_id)
+
+def unassoc_toy(request, turtle_id, toy_id):
+    Turtle.objects.get(id=turtle_id).toys.remove(toy_id)
+    return redirect('detail', turtle_id=turtle_id)
+
+def assoc_toy(request, turtle_id, toy_id):
+    Turtle.objects.get(id=turtle_id).toys.add(toy_id)
     return redirect('detail', turtle_id=turtle_id)
 
 def add_photo(request, turtle_id):
@@ -71,7 +81,7 @@ class ToyCreate(CreateView):
 
 class ToyUpdate(UpdateView):
     model = Toy
-    fields = ['name', 'color'
+    fields = ['name', 'color']
     
 class ToyDelete(DeleteView):
     model = Toy
